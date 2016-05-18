8f8adb000a8bc9e8fae98c7710d6cad1b788094a
Soichi Hayashi <hayashis@iu.edu>
2016-05-18 02:18:07 +0000

    switched to scp from odiuser

--- import.js
+++ import.js
@@ -3,6 +3,8 @@
 
 var request = require('request');
 var fs = require('fs');
+var async = require('async');
+var spawn = require('child_process').spawn;
 
 var config = require(process.cwd()+'/config.json');
 
@@ -14,15 +16,28 @@ var product = {exposures: []};
 
 var source = "/N/dc2/scratch/odiuser/SPIE_in";
 
+/* symlink
 for(var id in config.exposures) {
     var logical_id = config.exposures[id];
     fs.symlinkSync(source+"/"+logical_id, id);
     product.exposures.push(id);
 }
+*/
 
-fs.writeFile('products.json', JSON.stringify([product]), function(err) {
+//ssh from odiuser
+async.forEachOf(config.exposures, function(logical_id, id, next) {
+    console.log("handling "+logical_id);
+    var scp = spawn('scp -r -i ~/.ssh/odiuser.id_rsa odiuser@karst.uits.iu.edu:/N/dc2/scratch/odiuser/SPIE_in/'+logical_id+' '+id);    
+    scp.on('close', function(code) {
+        console.log("code "+code);
+        next();
+    });
+}, function(err) {
     if(err) throw err;
-    console.log("wrote products.json");
+    fs.writeFile('products.json', JSON.stringify([product]), function(err) {
+        if(err) throw err;
+        console.log("wrote products.json");
+    });
 });
 
 //config.exposure_ids
