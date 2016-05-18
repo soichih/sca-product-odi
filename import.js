f63adab6bcfd76b41ee29a2f73260ea68bb52ea6
Soichi Hayashi <hayashis@iu.edu>
2016-05-18 01:53:37 +0000

    implemented skelton data stager

--- import.js
+++ import.js
@@ -1,5 +1,30 @@
 #!/bin/env node
 'use strict';
 
+var request = require('request');
+var fs = require('fs');
+
 var config = require(process.cwd()+'/config.json');
-console.dir(config);
+
+//var url = config.odi_api.url;
+//var jwt = conodi_api.jwt;
+
+//request.post(config.odi_api.url+"/
+var product = {exposures: []};
+
+var source = "/N/dc2/scratch/odiuser/SPIE_in";
+
+for(var id in config.exposures) {
+    var logical_id = config.exposures[id];
+    fs.symlinkSync(source+"/"+logical_id, id);
+    product.exposures.push(id);
+}
+
+fs.writeFile('products.json', JSON.stringify([product]), function(err) {
+    if(err) throw err;
+    console.log("wrote products.json");
+});
+
+//config.exposure_ids
+
+
--- import.sh
+++ import.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 module load nodejs
+(cd $SCA_SERVICE_DIR && npm install)
+
 node $SCA_SERVICE_DIR/import
